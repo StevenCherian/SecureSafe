4b6ca1aedf389385da7e956be36f8732c0750128
Steven Cherian <stevencherian.va@gmail.com>
2021-11-18 00:39:27 -0500

    Added bottom nav bar, "my safe", and "settings page"

--- app/src/main/java/com/example/cmsc413project/SettingsActivity.java
+++ app/src/main/java/com/example/cmsc413project/SettingsActivity.java
@@ -7,25 +7,25 @@ import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_settings);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
-        bottomNavigationView.setSelectedItemId(R.id.home);
+        bottomNavigationView.setSelectedItemId(R.id.settings);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
             switch (item.getItemId())
             {
                 case R.id.home:
+                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                    overridePendingTransition(0,0);
                     return true;
 
                 case R.id.settings:
-                    startActivity(new Intent(getApplicationContext(),SettingsActivity.class));
-                    overridePendingTransition(0,0);
                     return true;
             }
             return false;
